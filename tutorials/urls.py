3cd9f816c703091cd77e7e75f1b099887078a0f7
Jennifer Sánchez Dueñas <jennifersduenas@Mac-mini-de-Jennifer.local>
2017-09-01 00:45:35 +0200

    create view details tutorial series

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^series/(?P<slug>[-\w]+)/$', views.tutorial_series_detail, name="tutorial_series_detail"),
+    #url(r'^series/(?P<slug>[-\w]+)/$', views.tutorial_series_detail, name="tutorial_series_detail"),
+    url(regex=r'^series/(?P<slug>[-\w]+)/$', view=views.TutorialSeriesDetailView.as_view(), name="tutorial_series_detail"),
 ]
--- tutorials/views.py
+++ tutorials/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
-from .models import TutorialSeries
+from .models import TutorialSeries, Lesson
 
-def tutorial_series_detail(request, slug):
-    object = get_object_or_404(TutorialSeries, slug=slug)
-    template = "tutorials/tutorialseries_detail.html"
-    context = {
-        'object': object,
-    }
-    return render(request, template, context)
+#def tutorial_series_detail(request, slug):
+#    object = get_object_or_404(TutorialSeries, slug=slug)
+#    template = "tutorials/tutorialseries_detail.html"
+#    context = {
+#        'object': object,
+#    }
+#    return render(request, template, context)
     
     
+class TutorialSeriesDetailView(DetailView):
+    model = TutorialSeries
+    
+    def get_context_data(self, **kwargs):
+        context = super(TutorialSeriesDetailView, self).get_context_data(**kwargs)
+        context['lessons'] = Lesson.objects.filter(tutorial_series=self.object)
+        return context
